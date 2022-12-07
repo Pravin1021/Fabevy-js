f18775b4ecd67b7f3cbd47eeba5ce948fc3dcdd7
Pravin1021 <mpravin1021@gmail.com>
2022-12-07 22:14:53 +0530

    add

--- form valid/script.js
+++ form valid/script.js
@@ -3,3 +3,10 @@ const username=document.getElementById("username")
 const email=document.getElementById("email")
 const password=document.getElementById("pass")
 const conpassword=document.getElementById("conpassword")
+
+let btn=document.getElementById("btn")
+btn.addEventListener("click",Myfunc)
+
+function Myfunc(){
+    
+}
